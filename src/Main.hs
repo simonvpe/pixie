9ae9e79c14ff0685caee165fed1dc7b1af3ed3ec
Simon Pettersson <simon.v.pettersson@gmail.com>
2020-10-11 17:46:09 +0200

    Simple lambda calculus example

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
-main :: IO()
-main = print "hello, world!";
+data Expr
+  = Num Integer
+  | Var Name
+  | Add Expr Expr
+  | Mul Expr Expr
+
+type Name = String
+
+main :: IO ()
+main = print "hello, world!"
