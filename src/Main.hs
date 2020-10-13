5751a4a94bf79bf0ee739bd03425302e0241f55e
Simon Pettersson <simon.v.pettersson@gmail.com>
2020-10-13 18:33:06 +0200

    Compiles

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,4 @@
 module Main where
 
-data Expr
-  = Num Integer
-  | Var Name
-  | Add Expr Expr
-  | Mul Expr Expr
-
-type Name = String
-
 main :: IO ()
-main = print "hello, world!"
+main = putStrLn "Hello, world"
